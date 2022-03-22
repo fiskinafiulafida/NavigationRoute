47ae468d7b08e1704c43a4eff25ab4b441488dac
fiskinafiulafida <fiskinafiul@gmail.com>
2022-03-22 15:40:52 +0700

    Item

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -12,28 +12,36 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text('Shopping List'),
+        ),
         body: Container(
-      margin: EdgeInsets.all(8),
-      child: ListView.builder(
-          padding: EdgeInsets.all(8),
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            final item = items[index];
-            return Card(
-              child: Container(
-                margin: EdgeInsets.all(8),
-                child: Row(children: [
-                  Expanded(child: Text(item.name)),
-                  Expanded(
-                    child: Text(
-                      item.price.toString(),
-                      textAlign: TextAlign.end,
+          margin: EdgeInsets.all(8),
+          child: ListView.builder(
+              padding: EdgeInsets.all(8),
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index];
+                return InkWell(
+                  onTap: () {
+                    Navigator.pushNamed(context, '/item', arguments: item);
+                  },
+                  child: Card(
+                    child: Container(
+                      margin: EdgeInsets.all(8),
+                      child: Row(children: [
+                        Expanded(child: Text(item.name)),
+                        Expanded(
+                          child: Text(
+                            item.price.toString(),
+                            textAlign: TextAlign.end,
+                          ),
+                        )
+                      ]),
                     ),
-                  )
-                ]),
-              ),
-            );
-          }),
-    ));
+                  ),
+                );
+              }),
+        ));
   }
 }
